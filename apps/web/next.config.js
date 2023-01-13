94ec3c8eaf36232ab18298bfecc7c7770c40070c
EL\max <max8hine@gmail.com>
2023-01-13 16:36:38 +1300

    update settings

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,9 @@
+const path = require("path");
+
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ["ui"],
   experimental: {
-    outputFileTracingRoot: path.join(__dirname, "../.."),
+    outputFileTracingRoot: path.join(__dirname, "../../"),
   },
 };
