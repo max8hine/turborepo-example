51a14632ffa0bc5bfe797d79c7adef492bcd4e10
EL\max <max8hine@gmail.com>
2023-01-20 08:35:48 +1300

    update tailwind config

---
+++ packages/config/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "../../packages/ui/**/*.{js,ts,jsx,tsx}",
+    "./**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
